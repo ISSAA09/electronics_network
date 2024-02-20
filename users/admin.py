35c905ccd10e360e70923a43debbd588d9bf9121
issaa09 <shafiyev29@gmail.com>
2024-02-20 13:59:47 +0300

    Initial

--- users/admin.py
+++ users/admin.py
@@ -8,5 +8,3 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'first_name', 'last_name', 'phone', 'is_active', 'role',)
     list_filter = ('is_active', 'role')
     search_fields = ('first_name', 'last_name',)
-
-
--- users/models.py
+++ users/models.py
@@ -1,10 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
 NULLABLE = {'blank': True, 'null': True}
 
 
